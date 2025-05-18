18b208a53a2f3c52ed07c215074f0f3bf0e4fdb7
ZacharyGrosswirth <zacharygrosswirth1@gmail.com>
2025-05-17 20:49:25 -0400

    basic navigation

--- ridethebus/src/App.js
+++ ridethebus/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
+import Lobby from './pages/Lobby';
+import WaitingRoom from './pages/Waiting';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Lobby />} />
+        <Route path="/waiting" element={<WaitingRoom />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ ridethebus/src/pages/Waiting.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WaitingRoom = () => {
+  return <h1>WaitingRoom</h1>;
+};
+
+export default WaitingRoom;
